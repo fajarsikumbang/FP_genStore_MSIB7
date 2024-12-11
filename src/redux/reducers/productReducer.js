6b3c059a7d08dfa94247261707cb14ae5879f787
fajarsikumbang <if22.fajarmaulana@mhs.ubpkarawang.ac.id>
2024-12-11 14:43:16 +0700

    Update ProductReducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -2,7 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const fetchProducts = createAsyncThunk("products/fetchProducts", async () => {
-    const response = await axios.get("https://fakestoreapi.com/products");
+    const response = await axios.get(process.env.REACT_APP_API_URL);
     return response.data;
 });
 
